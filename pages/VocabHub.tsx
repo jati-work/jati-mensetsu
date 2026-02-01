21add00e8850a66460f112fdb7e4c040b5da672d
jati-work <dwianritasejati@gmail.com>
2026-02-01 08:22:43 +0700

    Update VocabHub.tsx

--- pages/VocabHub.tsx
+++ pages/VocabHub.tsx
@@ -360,7 +360,7 @@ const masteredPercentage = filteredList.length > 0
             setShowCategorySuggestions(true);
         }}
         onFocus={() => setShowCategorySuggestions(true)}
-        className="bg-white p-5 rounded-2xl font-bold text-xs outline-none border-2 border-gray-100 w-full" 
+        className="bg-white p-5 rounded-2xl font-bold text-xs outline-none w-full"
         placeholder="Kategori" 
     />
     {showCategorySuggestions && newCategory && existingCategories.filter(cat => 
